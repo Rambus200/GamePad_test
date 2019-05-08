e2393cf96a3c2ea585c4ef8532c8eaa4e1a9f456
Rambus200 <50409536+Rambus200@users.noreply.github.com>
2019-05-08 09:02:19 -0400

    Initial commit

---
+++ Player.gd
@@ -0,0 +1,49 @@
+
+extends Area2D
+
+export var speed = 400 
+var screen_size  
+var debug_panel = false
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	screen_size = get_viewport_rect().size
+	
+	
+func _process(delta):
+
+	var velocity = Vector2()
+	if Input.is_action_just_pressed("ui_right"):
+		velocity.x +=1
+	if Input.is_action_just_pressed("ui_left"):
+			velocity.x -=1
+	if Input.is_action_just_pressed("ui_up"):
+			velocity.y -=1
+	if Input.is_action_just_pressed("ui_down"):
+			velocity.y +=1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+		
+		
+	$AnimatedSprite.position += velocity * delta
+	$AnimatedSprite.position.x = clamp($AnimatedSprite.position.x,0,screen_size.x)
+	$AnimatedSprite.position.y = clamp($AnimatedSprite.position.y,0,screen_size.y)
+	
+	if velocity.x > 0:
+		$AnimatedSprite.set_rotation(PI/2)
+	if velocity.x < 0:
+		$AnimatedSprite.set_rotation(-PI/2)
+	if velocity.y > 0:
+		$AnimatedSprite.flip_v = true
+	else:
+		$AnimatedSprite.flip_v = false
+		
+#	if (Input.is_action_just_pressed("Debug") && debug_panel== false):
+#		$Label.show()
+#		$Label.text = Engine.get_frames_per_second()
+#	else:
+#		$Label.hide()
