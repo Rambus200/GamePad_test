bc7f35fc5d162b2d4b0654fb7f45faa6f92edde4
Rambus200 <50409536+Rambus200@users.noreply.github.com>
2019-05-08 22:50:35 -0400

    hi

--- Player.gd
+++ Player.gd
@@ -5,43 +5,50 @@ export var speed = 400
 var screen_size  
 var debug_panel = false
 
+# Initialization
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
+	
 	screen_size = get_viewport_rect().size
 	
-	
+
+#Repeat code
 func _process(delta):
 
 	var velocity = Vector2()
-	if Input.is_action_just_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right"):
 		velocity.x +=1
-	if Input.is_action_just_pressed("ui_left"):
+	if Input.is_action_pressed("ui_left"):
 			velocity.x -=1
-	if Input.is_action_just_pressed("ui_up"):
+	if Input.is_action_pressed("ui_up"):
 			velocity.y -=1
-	if Input.is_action_just_pressed("ui_down"):
+	if Input.is_action_pressed("ui_down"):
 			velocity.y +=1
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		$AnimatedSprite.play()
-	else:
-		$AnimatedSprite.stop()
 		
-		
+	#Call animation_position Function
+	set_animation_position(velocity,delta)
+	#Call Movement Function
+	set_movement(velocity)
+
+#Methods
+
+func set_animation_position(velocity,delta):
 	$AnimatedSprite.position += velocity * delta
 	$AnimatedSprite.position.x = clamp($AnimatedSprite.position.x,0,screen_size.x)
 	$AnimatedSprite.position.y = clamp($AnimatedSprite.position.y,0,screen_size.y)
 	
+	#For Label position
+	$Label.rect_position.x = ($AnimatedSprite.position.x + 20)
+	$Label.rect_position.y = ($AnimatedSprite.position.y + 20)
+	#Casting position vector data to string
+	$Label.text = String($AnimatedSprite.position)
+	
+func get_animation_position():
+	return $AnimatedSprite.position
+
 	#Diagonal Movement
-#	if velocity.x > 0 && velocity.y > 0:
-#		$AnimatedSprite.set_rotation(PI/4)
-#	if velocity.x > 0 && velocity.y < 0:
-#		$AnimatedSprite.set_rotation(-PI/4)
-#	if velocity.x < 0 && velocity.y > 0:
-#		$AnimatedSprite.set_rotation((3*PI)/4)
-#	if velocity.x < 0 && velocity.y < 0:
-#		$AnimatedSprite.set_rotation(-(3*PI)/4)
+func set_movement(velocity):
 	
 	#Vert/Horz Movement
 	if velocity.x > 0:
@@ -52,6 +59,15 @@ func _process(delta):
 		$AnimatedSprite.set_rotation(-PI)
 	if velocity.y < 0:
 		$AnimatedSprite.set_rotation(0)
+		
+	if (velocity.x > 0 && velocity.y > 0):
+		$AnimatedSprite.set_rotation((3*PI)/4)
+	if velocity.x > 0 && velocity.y < 0:
+		$AnimatedSprite.set_rotation(PI/4)
+	if velocity.x < 0 && velocity.y > 0:
+		$AnimatedSprite.set_rotation(-(3*PI)/4)
+	if velocity.x < 0 && velocity.y < 0:
+		$AnimatedSprite.set_rotation(-PI/4)
 		
 #	if (Input.is_action_just_pressed("Debug") && debug_panel== false):
 #		$Label.show()
