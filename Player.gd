325d7fcb3477f4f3725281fc3e044bb5b19db14a
Rambus200 <50409536+Rambus200@users.noreply.github.com>
2019-05-12 10:32:06 -0400

    hi

--- Player.gd
+++ Player.gd
@@ -1,6 +1,7 @@
 
 extends Area2D
 signal hit
+signal exit_key
 
 export var speed = 400 
 export var angle_rate = 0.1
@@ -15,8 +16,9 @@ func _ready():
 	
 #Repeat code
 func _process(delta):
-
 	var velocity = Vector2()
+	if Input.is_action_just_pressed("Esc"):
+		emit_signal("exit_key")
 	if Input.is_action_pressed("ui_right"):
 		velocity.x +=1
 	if Input.is_action_pressed("ui_left"):
@@ -30,6 +32,7 @@ func _process(delta):
 		$Particles2D.emitting = true
 	else:
 		$Particles2D.emitting = false
+	
 		
 	#Call animation_position Function
 	set_animation_position(velocity,delta)
@@ -40,22 +43,22 @@ func _process(delta):
 #Methods
 
 func set_animation_position(velocity,delta):
-	$AnimatedSprite.position += velocity * delta
-	$AnimatedSprite.position.x = clamp($AnimatedSprite.position.x,0,screen_size.x)
-	$AnimatedSprite.position.y = clamp($AnimatedSprite.position.y,0,screen_size.y)
+	position += velocity * delta
+	position.x = clamp(position.x,0,screen_size.x)
+	position.y = clamp(position.y,0,screen_size.y)
 	
 	#For Label position
 	$Label.rect_position.x = ($AnimatedSprite.position.x + 20)
 	$Label.rect_position.y = ($AnimatedSprite.position.y + 20)
 	#Casting position vector data to string
-	$Label.text = String($AnimatedSprite.position)
+	$Label.text = String(position)
 	#Position of Particles2D
 	$Particles2D.position.x = ($AnimatedSprite.position.x)
 	$Particles2D.position.y = ($AnimatedSprite.position.y)
 	$Particles2D.local_coords = true
 	
 func get_animation_position():
-	return $AnimatedSprite.position
+	return position
 	
 	#Diagonal Movement
 func set_movement(velocity):
